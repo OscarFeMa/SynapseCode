79fc2036728a2f3160057592c4fba9beaf66b8a1
OscarFeMa <makeder@gmail.com>
2026-05-16 12:17:28 +0200

    feat: SynapseCode Control Center v2.7 - full React dashboard with 6 tabs

---
+++ open_control_center.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ========================================
+echo   SynapseCode Control Center v2.7
+echo ========================================
+echo.
+echo Opening Control Center in default browser...
+echo.
+start "" "%~dp0frontend\control-center\index.html"
+echo Done!
+pause
