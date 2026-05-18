1e3f12e301b00e0d3a8ff23acbd6a25ad05d160d
OscarFeMa <makeder@gmail.com>
2026-05-18 21:29:46 +0200

    feat: add sonner toast notifications system

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'sonner'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Toaster
+      position="bottom-right"
+      toastOptions={{
+        style: {
+          background: '#0f172a',
+          border: '1px solid #1e293b',
+          color: '#e2e8f0',
+        },
+      }}
+    />
   </React.StrictMode>,
 )
